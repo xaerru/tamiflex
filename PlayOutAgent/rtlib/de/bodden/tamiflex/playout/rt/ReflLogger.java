d80adebb636e44db84612ad8cc0e18fd71277cc8
Gauravsingh Sisodia <xaerru@gmail.com>
2026-02-26 11:35:35 +0000

    feat: instrument Class.getMethods() to sort before returning

--- PlayOutAgent/rtlib/de/bodden/tamiflex/playout/rt/ReflLogger.java
+++ PlayOutAgent/rtlib/de/bodden/tamiflex/playout/rt/ReflLogger.java
@@ -42,7 +42,9 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -279,6 +281,40 @@ public class ReflLogger {
         }
 
         return byteCodeToReturn;
+    }
+
+    public static Method[] sortMethods(Method[] m) {
+        if (m == null || m.length <= 1) {
+            return m;
+        }
+
+        Method[] sorted = m.clone();
+
+        Arrays.sort(sorted, new Comparator<Method>() {
+            @Override
+            public int compare(Method m1, Method m2) {
+                int val = m1.getName().compareTo(m2.getName());
+                if (val != 0) return val;
+
+                val = Integer.compare(m1.getParameterCount(), m2.getParameterCount());
+                if (val != 0) return val;
+
+                Class<?>[] params1 = m1.getParameterTypes();
+                Class<?>[] params2 = m2.getParameterTypes();
+                for (int i = 0; i < params1.length; i++) {
+                    String p1 = params1[i].getName();
+                    String p2 = params2[i].getName();
+                    val = p1.compareTo(p2);
+                    if (val != 0) return val;
+                }
+
+                String r1 = m1.getReturnType().getName();
+                String r2 = m2.getReturnType().getName();
+                return r1.compareTo(r2);
+            }
+        });
+
+        return sorted;
     }
 
     public static String tryGetHiddenHashedName(Class<?> clazz) {
---
+++ PlayOutAgent/src/de/bodden/tamiflex/playout/transformation/invoke/AbstractGetMethodsTransformation.java
@@ -0,0 +1,39 @@
+package de.bodden.tamiflex.playout.transformation.invoke;
+
+import static org.objectweb.asm.Opcodes.ARETURN;
+
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.commons.Method;
+
+import de.bodden.tamiflex.playout.transformation.AbstractTransformation;
+import de.bodden.tamiflex.playout.Agent;
+
+
+public abstract class AbstractGetMethodsTransformation extends AbstractTransformation {
+	
+	public AbstractGetMethodsTransformation(Method... methods) throws Exception {
+		super(Class.forName("java.lang.Class"), methods);
+	}
+	
+	@Override
+	protected MethodVisitor getMethodVisitor(MethodVisitor parent) 
+    {
+        return new MethodVisitor(ASM9, parent) {
+
+            @Override
+            public void visitInsn(int opcode) {
+                if (opcode == ARETURN) {
+                    super.visitMethodInsn(INVOKESTATIC,
+                        "de/bodden/tamiflex/playout/rt/ReflLogger",
+                        methodName(),
+                        methodSignature(),
+                        false);
+                }
+                super.visitInsn(opcode);
+            };
+		};
+	}
+	protected abstract String methodName();
+
+	protected abstract String methodSignature();
+}
